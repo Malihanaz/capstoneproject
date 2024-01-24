79f77f25d9976951ec49e4e210e18b19d213522a
Malihanaz <gmalihanaz@gmail.com>
2024-01-24 20:14:59 +0530

    capstone project

---
+++ BlogTrackerAPI/Data/BlogDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BlogTrackerAPI.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogTrackerAPI.Models.BlogInfo> BlogInfo { get; set; } = default!;
+
+        public DbSet<BlogTrackerAPI.Models.EmpInfo>? EmpInfo { get; set; }
+        public DbSet<BlogTrackerAPI.Models.AdminInfo> AdminInfo { get; set; }
+
+        internal Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
